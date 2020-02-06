ddd9a978b44b41d480f3d409ffbb411dc07be738
Travis Holloway <travisinseattle@gmail.com>
2020-02-06 13:27:22 -0800

    SQL Challenge: Done

--- sql/inventory_reconciliation.sql
+++ sql/inventory_reconciliation.sql
@@ -1,15 +1,15 @@
 --Create a table to hold inventory balances:
 declare @inventory table (
-    ItemNumber varchar(50) not null,
+    ItemNumber varchar(50)  not null,
     WarehouseLocation varchar(50) not null,
     QuantityOnHand int not null,
-    PricePerItem decimal not null
+    PricePerItem decimal(18,2) not null
 )
 
 --Create a table to hold accounting balances: 
 declare @accounting table (
-    ItemNumber varchar(50) not null,
-    TotalInventoryValue decimal not null
+    ItemNumber varchar(50)  not null,
+    TotalAccountingValue decimal(18,2) not null
 )
 
 --Mock up some inventory balances
@@ -27,10 +27,27 @@ INSERT INTO @accounting VALUES ('xxccM', 7602.75)
 INSERT INTO @accounting VALUES ('fbr77', 17.99)
 
 --TODO-CHALLENGE: Write a query to reconcile matches/differences between the inventory and accounting tables
-select I.ItemNumber, TRY_CAST(SUM(TRY_CAST((I.PricePerItem * I.QuantityOnHand) AS DECIMAL)) AS DECIMAL) as TotalValue, A.TotalInventoryValue, A.ItemNumber
+
+-- Needed to keep the decimals as decimals, otherwise they rounded up.
+SET NUMERIC_ROUNDABORT OFF 
+
+-- This code would have been much easier if I wasn't fighting the collation so much.
+-- By default the server collation is case insensitive which really messes up sums for ItemNumbers
+-- that are identical except for case.
+
+select I.ItemNumber AS InventoryItemNumber,
+    A.ItemNumber AS AccountingItemNumber,
+    TotalInventoryValue,
+    A.TotalAccountingValue,
+    CASE WHEN TotalInventoryValue = A.TotalAccountingValue Then 'Yes' ELSE 'No' end AS Is_Balanced
 FROM @inventory AS I
+JOIN (SELECT
+    ItemNumber COLLATE Latin1_General_CS_AI AS ItemNumber,
+    SUM((PricePerItem * QuantityOnHand)) as TotalInventoryValue
+    FROM @inventory
+    group by ItemNumber COLLATE Latin1_General_CS_AI) as inv
+    ON inv.ItemNumber = I.ItemNumber
 FULL OUTER JOIN @accounting as A
 ON I.ItemNumber = A.ItemNumber
-GROUP BY I.ItemNumber, A.TotalInventoryValue, A.ItemNumber
-ORDER BY I.ItemNumber COLLATE Latin1_General_CS_AI;
-
+GROUP BY I.ItemNumber, A.TotalAccountingValue, A.ItemNumber, TotalInventoryValue
+order by I.ItemNumber;
