5b71ff376566f5ad1c7716d2e480093140aee12b
Travis Holloway <travisinseattle@gmail.com>
2020-02-06 10:55:58 -0800

    Generated tests: Done.

--- csharp/src/Bargreen.Services/InventoryService.cs
+++ csharp/src/Bargreen.Services/InventoryService.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using System.Linq;
 using System.Globalization;
 using System.Text;
 
@@ -115,58 +116,128 @@ namespace Bargreen.Services
             });
         }
 
+        /* Notes:
+            I had considered writing out async helper methods so each of the two lists could be operated on independently.  I eventually decided
+            against it as that would require extra runs through each list to consolidate them, and I was trying to keep this method as fast as possible.
+            What makes this O(n+m) is that only one pass through each list needs to be performed, but only if done synchronously.  (the calls to Count()
+            don't count (lol) as it most likely returns a private count variable instead of iterating through the list)
+            Finally, it seemed to me that in an API, something is better than nothing, so returning a result if one or more lists were empty was a
+            better option than throwing errors.  Without knowing the greater scope of the program, it's a bit of a tossup as to what the better
+            practice would be.*/
+
         public async Task<IEnumerable<InventoryReconciliationResult>> ReconcileInventoryToAccounting(IEnumerable<InventoryBalance> inventoryBalances, IEnumerable<AccountingBalance> accountingBalances)
         {
             /* We can't do nested loops, as that runs in O(n*m), which is not ideal.  Instead, we create a dictionary
              using the inventory enumerable and run a loop over the accounting enumerable to check against it.*/
             var lookup = new Dictionary<string, InventoryReconciliationResult>();
+            IEnumerable<InventoryReconciliationResult> result;
             InventoryReconciliationResult temp; // Should this be kept local to the loop to avoid unpredictiable behavior?
 
             return await Task.Run(() => {
-                // Iterate over the inventory balances list to create a dictionary. (O(n))
-                foreach (var item in inventoryBalances)
+                //Only proceed if both lists are populated.
+                if (inventoryBalances.Count() > 0 && accountingBalances.Count() > 0)
                 {
-                    if (lookup.TryGetValue(item.ItemNumber, out temp))
+                    // If we have entries, iterate over the inventory balances list to create a dictionary. (O(n))
+                    foreach (var item in inventoryBalances)
                     {
-                        temp.TotalValueOnHandInInventory += item.PricePerItem * item.QuantityOnHand;
-                    }
-                    else
-                    {
-                        lookup.Add(item.ItemNumber, new InventoryReconciliationResult()
+                        if (lookup.TryGetValue(item.ItemNumber, out temp))
                         {
-                            ItemNumber = item.ItemNumber,
-                            TotalValueOnHandInInventory = item.PricePerItem * item.QuantityOnHand,
-                            TotalValueInAccountingBalance = 0.0M,
-                            inventoryIsBalanced = false
-                        });
+                            temp.TotalValueOnHandInInventory += item.PricePerItem * item.QuantityOnHand;
+                        }
+                        else
+                        {
+                            lookup.Add(item.ItemNumber, new InventoryReconciliationResult()
+                            {
+                                ItemNumber = item.ItemNumber,
+                                TotalValueOnHandInInventory = item.PricePerItem * item.QuantityOnHand,
+                                TotalValueInAccountingBalance = 0.0M,
+                                inventoryIsBalanced = false
+                            });
+                        }
                     }
+
+                    // while iterating over accounting, add or update entries in the directory. (O(m))
+                    foreach (var item in accountingBalances)
+                    {
+                        // If a match is found, update entry and decide if it is balanced.
+                        if (lookup.TryGetValue(item.ItemNumber, out temp))
+                        {
+                            // First, update TotalValueInAccountingBalance and then set the boolean.
+                            temp.TotalValueInAccountingBalance = item.TotalInventoryValue;
+                            temp.inventoryIsBalanced = temp.TotalValueOnHandInInventory == item.TotalInventoryValue;
+                        }
+                        else
+                        {
+                            // Create an new entry in the directory since there is an entry in accounting that is not in inventory.
+                            lookup.Add(item.ItemNumber, new InventoryReconciliationResult()
+                            {
+                                ItemNumber = item.ItemNumber,
+                                TotalValueOnHandInInventory = 0.0M,
+                                TotalValueInAccountingBalance = item.TotalInventoryValue,
+                                inventoryIsBalanced = false
+                            });
+                        }
+                    }
+                    // return the values of the directory for a run time of O(n+m).
+                    result = lookup.Values.ToList<InventoryReconciliationResult>();
+                }
+                else
+                // attempt to return something in the case of an empty list parameter, in case there still results in the other list.
+                {
+                    // If inventory has entries, add them to the results dictionary.
+                    if (inventoryBalances.Count() > 0)
+                    {
+                        foreach (var item in inventoryBalances)
+                        {
+                            if (lookup.TryGetValue(item.ItemNumber, out temp))
+                            {
+                                temp.TotalValueOnHandInInventory += item.PricePerItem * item.QuantityOnHand;
+                            }
+                            else
+                            {
+                                lookup.Add(item.ItemNumber, new InventoryReconciliationResult()
+                                {
+                                    ItemNumber = item.ItemNumber,
+                                    TotalValueOnHandInInventory = item.PricePerItem * item.QuantityOnHand,
+                                    TotalValueInAccountingBalance = 0.0M,
+                                    inventoryIsBalanced = false
+                                });
+                            }
+                        }
+                    }
+
+                    // If accounting has entries, add them to the results dictionary.
+                    if (accountingBalances.Count() > 0)
+                    {
+                        // If not empty, iterate over accounting, add entries to the result dictionary. (O(m))
+                        foreach (var item in accountingBalances)
+                        {
+                            // If a match is found, update entry and decide if it is balanced.
+                            if (lookup.TryGetValue(item.ItemNumber, out temp))
+                            {
+                                // First, update TotalValueInAccountingBalance and then set the boolean.
+                                temp.TotalValueInAccountingBalance = item.TotalInventoryValue;
+                                temp.inventoryIsBalanced = temp.TotalValueOnHandInInventory == item.TotalInventoryValue;
+                            }
+                            else
+                            {
+                                // Create an new entry in the directory since there is an entry in accounting that is not in inventory.
+                                lookup.Add(item.ItemNumber, new InventoryReconciliationResult()
+                                {
+                                    ItemNumber = item.ItemNumber,
+                                    TotalValueOnHandInInventory = 0.0M,
+                                    TotalValueInAccountingBalance = item.TotalInventoryValue,
+                                    inventoryIsBalanced = false
+                                });
+                            }
+                        }
+                    }
+
+                    result = lookup.Values.ToList<InventoryReconciliationResult>();
                 }
 
-                // while iterating over accounting, add or update entries in the directory. (O(m))
-                foreach (var item in accountingBalances)
-                {
-                    // If a match is found, update entry and decide if it is balanced.
-                    if (lookup.TryGetValue(item.ItemNumber, out temp))
-                    {
-                        // First, update TotalValueInAccountingBalance and then set the boolean.
-                        temp.TotalValueInAccountingBalance = item.TotalInventoryValue;
-                        temp.inventoryIsBalanced = temp.TotalValueOnHandInInventory == item.TotalInventoryValue;
-                    }
-                    else
-                    {
-                        // Create an new entry in the directory since there is an entry in accounting that is not in inventory.
-                        lookup.Add(item.ItemNumber, new InventoryReconciliationResult()
-                        {
-                            ItemNumber = item.ItemNumber,
-                            TotalValueOnHandInInventory = 0.0M,
-                            TotalValueInAccountingBalance = item.TotalInventoryValue,
-                            inventoryIsBalanced = false
-                        });
-                    }
-                }
-
-                // return the values of the directory for a run time of O(n+m).
-                return lookup.Values;
+                // We are done, return whatever we have.
+                return result;
             });
         }
     }
--- csharp/src/Bargreen.Tests/InventoryServiceTests.cs
+++ csharp/src/Bargreen.Tests/InventoryServiceTests.cs
@@ -1,4 +1,10 @@
+using Bargreen.Services;
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
 using Xunit;
 
 namespace Bargreen.Tests
@@ -6,10 +12,84 @@ namespace Bargreen.Tests
     public class InventoryServiceTests
     {
         [Fact]
-        public void Inventory_Reconciliation_Performs_As_Expected()
+        // Test that Async has been acheived in all methods.
+        public void Service_Methods_Are_Async()
         {
-            //TODO-CHALLENGE: Verify expected output of your recon algorithm. Note, this will probably take more than one test
-            throw new NotImplementedException();
+            var methods = typeof(InventoryService)
+                .GetMethods()
+                .Where(m => m.DeclaringType == typeof(InventoryService));
+
+            Assert.All(methods, m =>
+            {
+                Type attType = typeof(AsyncStateMachineAttribute);
+                var attrib = (AsyncStateMachineAttribute)m.GetCustomAttribute(attType);
+                Assert.NotNull(attrib);
+                Assert.Equal(typeof(Task), m.ReturnType.BaseType);
+            });
+        }
+
+        [Fact]
+        // There are no parameters to pass, so the method needs to return a non-null List<InventoryBalance>.
+        public async void GetInventoryBalance_Performs_As_Expected()
+        {
+            var service = new InventoryService();
+            var result = await service.GetInventoryBalances();
+            Assert.NotEmpty(result);
+            Assert.IsType<List<InventoryBalance>>(result); // Perhaps unnecessary in a strongly typed language?  I have spent a long time in Javascript land.
+        }
+
+        [Fact]
+        // There are no parameters to pass, so the method needs to return a non-null List<InventoryBalance>.
+        public async void GetAccountingBalance_Performs_As_Expected()
+        {
+            var service = new InventoryService();
+            var result = await service.GetAccountingBalances();
+            Assert.NotEmpty(result);
+            Assert.IsType<List<AccountingBalance>>(result);
+        }
+
+        // Two Parameters, we need test what happens when either are null or empty and that proper input produces proper results.
+        [Fact]
+        public async void Null_Inventory_List_Throws_Error()
+        {
+            var service = new InventoryService();
+            var result = await Assert.ThrowsAsync<ArgumentNullException>(async () =>
+                await service.ReconcileInventoryToAccounting(null, await service.GetAccountingBalances()));
+        }
+
+        [Fact]
+        public async void Null_Accounting_List_Throws_Error()
+        {
+            var service = new InventoryService();
+            var result = await Assert.ThrowsAsync<ArgumentNullException>(async () =>
+                await service.ReconcileInventoryToAccounting(await service.GetInventoryBalances(), null));
+        }
+
+        [Fact]
+        public async void Empty_Inventory_List_Returns_Accounting_Results()
+        {
+            var service = new InventoryService();
+            var result = await service.ReconcileInventoryToAccounting(new List<InventoryBalance>(), await service.GetAccountingBalances());
+            Assert.NotEmpty(result);
+            Assert.IsType<List<InventoryReconciliationResult>>(result);
+        }
+
+        [Fact]
+        public async void Empty_Accounting_List_Returns_Inventory_Results()
+        {
+            var service = new InventoryService();
+            var result = await service.ReconcileInventoryToAccounting(await service.GetInventoryBalances(), new List<AccountingBalance>());
+            Assert.NotEmpty(result);
+            Assert.IsType<List<InventoryReconciliationResult>>(result);
+        }
+
+        [Fact]
+        public async void Inventory_Reconciliation_Performs_As_Expected()
+        {
+            var service = new InventoryService();
+            var result = await service.ReconcileInventoryToAccounting(await service.GetInventoryBalances(), await service.GetAccountingBalances());
+            Assert.NotEmpty(result);
+            Assert.IsType<List<InventoryReconciliationResult>>(result);
         }
     }
 }
