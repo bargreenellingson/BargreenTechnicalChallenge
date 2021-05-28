02a0eda18a66dfa96ef5ebe14c0c3875ef5da8e8
Valeria Avila <valeria.avila17@gmail.com>
2021-05-28 01:20:32 -0700

    I dont have much experience with unit testing

--- csharp/src/Bargreen.Tests/InventoryServiceTests.cs
+++ csharp/src/Bargreen.Tests/InventoryServiceTests.cs
@@ -1,3 +1,4 @@
+using Bargreen.Services;
 using System;
 using Xunit;
 
@@ -5,10 +6,13 @@ namespace Bargreen.Tests
 {
     public class InventoryServiceTests
     {
+        private InventoryService InventoryService;
+
         [Fact]
+
         public void Inventory_Reconciliation_Performs_As_Expected()
         {
-            //TODO-CHALLENGE: Verify expected output of your recon algorithm. Note, this will probably take more than one test
+
             throw new NotImplementedException();
         }
     }
