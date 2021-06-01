19194207e0b4b33545613e144d677e869179f9d7
Joel Tennant <joel_tennant@yahoo.com>
2021-05-31 17:50:39 -0700

    Implemented GetReconciliation method

--- csharp/src/Bargreen.Services/InventoryService.cs
+++ csharp/src/Bargreen.Services/InventoryService.cs
@@ -2,6 +2,7 @@
 using Bargreen.Services.Interfaces;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Bargreen.Services
@@ -11,7 +12,38 @@ namespace Bargreen.Services
         public IEnumerable<InventoryReconciliationResult> ReconcileInventoryToAccounting(IEnumerable<InventoryBalance> inventoryBalances, IEnumerable<AccountingBalance> accountingBalances)
         {
             //TODO-CHALLENGE: Compare inventory balances to accounting balances and find differences
-            throw new NotImplementedException();
+            Dictionary<string, Decimal[]> result = new Dictionary<string, Decimal[]>();
+
+            foreach (var inv in inventoryBalances)
+            {
+                if (!result.ContainsKey(inv.ItemNumber.ToUpper()))
+                {
+                    result.Add(inv.ItemNumber, new decimal[] { inv.PricePerItem * inv.QuantityOnHand, 0.00M });
+                }
+                else
+                {
+                    result[inv.ItemNumber.ToUpper()][0] += inv.PricePerItem * inv.QuantityOnHand;
+                }
+            }
+
+            foreach (var act in accountingBalances)
+            {
+                if (result.ContainsKey(act.ItemNumber))
+                {
+                    result[act.ItemNumber][1] = act.TotalInventoryValue;
+                }
+                else
+                {
+                    result.Add(act.ItemNumber, new decimal[] { 0.00M, act.TotalInventoryValue });
+                }
+            }
+
+            return result.Select(r => new InventoryReconciliationResult
+            {
+                ItemNumber = r.Key,
+                TotalValueOnHandInInventory = r.Value[0],
+                TotalValueInAccountingBalance = r.Value[1]
+            }).ToList();
         }
     }
 }
