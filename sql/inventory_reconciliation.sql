a3b79b3c471eeac11083cec43a5c989b123eb2a0
Joel Tennant <joel_tennant@yahoo.com>
2021-05-31 18:18:56 -0700

    Completed SQL assessment

--- sql/inventory_reconciliation.sql
+++ sql/inventory_reconciliation.sql
@@ -3,13 +3,13 @@ declare @inventory table (
     ItemNumber varchar(50) not null,
     WarehouseLocation varchar(50) not null,
     QuantityOnHand int not null,
-    PricePerItem decimal not null
+    PricePerItem decimal(18, 2) not null --Value was getting rounded up without this fix - I noticed when I was hand checking the results
 )
 
 --Create a table to hold accounting balances: 
 declare @accounting table (
     ItemNumber varchar(50) not null,
-    TotalInventoryValue decimal not null
+    TotalInventoryValue decimal(18, 2) not null
 )
 
 --Mock up some inventory balances
@@ -27,4 +27,28 @@ INSERT INTO @accounting VALUES ('xxccM', 7602.75)
 INSERT INTO @accounting VALUES ('fbr77', 17.99)
 
 --TODO-CHALLENGE: Write a query to reconcile matches/differences between the inventory and accounting tables
-SELECT * FROM ...
+/*
+	Y = Yes
+	N = No
+*/
+SELECT CASE
+           WHEN [inv].[ItemNumber] IS NOT NULL THEN [inv].[ItemNumber]
+           ELSE [act].[ItemNumber]
+       END AS [ItemNumber]
+     , ISNULL([inv].[QuantityOnHand], 0) AS [QuantityOnHand]
+     , ISNULL([inv].[PricePerItem] * [inv].[QuantityOnHand], 0) [QuantityOnHandValue]
+     , ISNULL([act].[TotalInventoryValue], 0) AS [AccountingInventoryValue]
+     , CASE
+           WHEN ISNULL([inv].[PricePerItem] * [inv].[QuantityOnHand], 0) <> ISNULL([act].[TotalInventoryValue], 0) THEN
+               'N'
+           ELSE 'Y'
+       END AS [Match]
+FROM
+    (SELECT [i].[ItemNumber]
+          , [i].[PricePerItem]
+          , SUM([i].[QuantityOnHand]) [QuantityOnHand]
+     FROM @inventory AS [i]
+     GROUP BY [i].[ItemNumber]
+            , [i].[PricePerItem]) AS [inv]
+    FULL OUTER JOIN @accounting AS [act] ON [act].[ItemNumber] = [inv].[ItemNumber]
+ORDER BY [Match] DESC
