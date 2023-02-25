d1493536db02153bdd9e90ef8215a7fa4a9dcd64
Omar Iqbal <omar.iqbal.395@gmail.com>
2023-02-25 00:03:47 -0800

    Added a few more unit tests

--- csharp/src/Bargreen.Tests/InventoryServiceTests.cs
+++ csharp/src/Bargreen.Tests/InventoryServiceTests.cs
@@ -67,5 +67,84 @@ namespace Bargreen.Tests
             Assert.NotEmpty(result);
             Assert.Equal(2, result.Count());
         }
+
+        /**
+         * this is a case where the inventoryOnHand is empty and compared to the AccountingBalances which is not empty
+         * expected output should be exact copy of AccountingBalances
+         * **/
+        [Fact]
+        public void Inventory_Reconciliation_No_OnHandInventory()
+        {
+            List<AccountingBalance> accountingsBalance = new List<AccountingBalance>()
+            {
+                new AccountingBalance()
+                {
+                     ItemNumber = "ABC123",
+                     TotalInventoryValue = 3435M
+                },
+                new AccountingBalance()
+                {
+                     ItemNumber = "ZZZ99",
+                     TotalInventoryValue = 1930.62M
+                },
+                new AccountingBalance()
+                {
+                     ItemNumber = "xxccM",
+                     TotalInventoryValue = 7602.75M
+                }
+            };
+            inventoryService = new InventoryService(accountingsBalance);
+            IEnumerable<InventoryBalance> inventoryList = inventoryService.GetInventoryBalances();
+            IEnumerable<AccountingBalance> accountingList = inventoryService.GetAccountingBalances();
+
+            IEnumerable<InventoryReconciliationResult> result = inventoryService.ReconcileInventoryToAccounting(inventoryList, accountingList);
+
+
+            Assert.NotNull(result);
+            Assert.NotEmpty(result);
+            Assert.Equal(3, result.Count());
+        }
+
+        /**
+         * this is a case where the inventoryOnHand is compared to the AccountingBalances and they are the same (valid case)
+         * expected output should be an empty IEnumerable
+         * **/
+        [Fact]
+        public void Inventory_Reconciliation_Happy_Path()
+        {
+            List<InventoryBalance> inventoryOnHandBalances = new List<InventoryBalance>()
+            {
+                new InventoryBalance()
+                {
+                     ItemNumber = "ABC123",
+                     PricePerItem = 7.5M,
+                     QuantityOnHand = 312,
+                     WarehouseLocation = "WLA1"
+                },
+                new InventoryBalance()
+                {
+                     ItemNumber = "ABC123",
+                     PricePerItem = 7.5M,
+                     QuantityOnHand = 146,
+                     WarehouseLocation = "WLA2"
+                }
+            };
+
+            List<AccountingBalance> accountingsBalance = new List<AccountingBalance>()
+            {
+                new AccountingBalance()
+                {
+                     ItemNumber = "ABC123",
+                     TotalInventoryValue = 3435M
+                }
+            };
+            inventoryService = new InventoryService(inventoryOnHandBalances, accountingsBalance);
+            IEnumerable<InventoryBalance> inventoryList = inventoryService.GetInventoryBalances();
+            IEnumerable<AccountingBalance> accountingList = inventoryService.GetAccountingBalances();
+
+            IEnumerable<InventoryReconciliationResult> result = inventoryService.ReconcileInventoryToAccounting(inventoryList, accountingList);
+
+            Assert.Empty(result);
+        }
     }
 }
